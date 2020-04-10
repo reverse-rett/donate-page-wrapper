f8b2acb61417c608c65ccdfee3c0bb6b5e275b20
lluismather <lluismather@gmail.com>
2020-04-10 11:46:02 +0100

    styles initial

--- donate.js
+++ donate.js
@@ -1,3 +1,9 @@
 
-console.log("this has worked!")
-alert('This is the script working')
+// position header
+$('.__layout_header_inner').css('text-align', 'center');
+
+// make the form 60% width and center
+$('.__layout_page').css('width', '60%');
+$('.__layout_page').css('margin', '0 auto');
+$('.__layout_page').css('background-color', '#322a7e');
+$('.__layout_page').css('color', '#fff');
